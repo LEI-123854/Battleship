e2bd9e1e4cb43b49b6c7bc282f88bc6e841f74bb
tiago92nogueira <sohono2005@gmail.com>
2025-11-13 17:18:23 +0000

    testes relativos a bussola e posicao lei122693

--- src/test/java/iscteiul/ista/battleship/PositionTest.java
+++ src/test/java/iscteiul/ista/battleship/PositionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//a
 class PositionTest {
 
     private Position p1;
